d4e2d9fcf053eed2dfd0be9e7ff962fe9ff55b10
SeanSin <seansin@chungbuk.ac.kr>
2020-07-16 19:33:58 +0900

    study: Spring JPA & H2 DB Connection description : 스프링 JPA와 H2 DB 콘솔 연동이 제대로 되는지에 대한 테스트 코드

---
+++ jpashop/src/test/java/jpashop/jpabook/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package jpashop.jpabook;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUserName("MemberB");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
