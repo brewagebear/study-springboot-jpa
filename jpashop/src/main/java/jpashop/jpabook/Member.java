00975593dfe5c93f01f18dbff8e4fee44e84c612
SeanSin <seansin@chungbuk.ac.kr>
2020-07-16 19:33:32 +0900

    study: Spring JPA & H2 DB Connection description : 스프링 JPA와 H2 DB 콘솔의 연동을 위한 엔티티 클래스 작성

---
+++ jpashop/src/main/java/jpashop/jpabook/Member.java
@@ -0,0 +1,21 @@
+package jpashop.jpabook;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+
+}
