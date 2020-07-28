4097f6611096a8b8de29ce8890177fbe9c69c39c
SeanSin <seansin@chungbuk.ac.kr>
2020-07-28 16:42:33 +0900

    study: implemantation item service & repository code

--- jpashop/src/main/java/jpashop/jpabook/section3/service/ItemService.java
+++ jpashop/src/main/java/jpashop/jpabook/section3/service/ItemService.java
@@ -1,4 +1,31 @@
 package jpashop.jpabook.section3.service;
 
+import jpashop.jpabook.section2.domain.item.Item;
+import jpashop.jpabook.section3.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long saveItem(Item item){
+        return itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
 }
