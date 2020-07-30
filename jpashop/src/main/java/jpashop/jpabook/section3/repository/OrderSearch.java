18c93aa863a6b2f56e820dfa889c74ce4500614f
SeanSin <seansin@chungbuk.ac.kr>
2020-07-30 16:49:25 +0900

    study : fix querydsl error

--- jpashop/src/main/java/jpashop/jpabook/section3/repository/OrderSearch.java
+++ jpashop/src/main/java/jpashop/jpabook/section3/repository/OrderSearch.java
@@ -3,7 +3,6 @@ package jpashop.jpabook.section3.repository;
 import jpashop.jpabook.section2.domain.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Service;
 
 @Getter
 @Setter
