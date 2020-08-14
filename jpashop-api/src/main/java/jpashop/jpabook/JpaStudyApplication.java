73a7914bdf8552308d0f24a30c94f60d17b464aa
SeanSin <seansin@chungbuk.ac.kr>
2020-08-14 17:11:22 +0900

    study2: fix hibernate5Module configure
    hibernate5Module configure 사용안하게 수정

--- jpashop-api/src/main/java/jpashop/jpabook/JpaStudyApplication.java
+++ jpashop-api/src/main/java/jpashop/jpabook/JpaStudyApplication.java
@@ -1,7 +1,9 @@
 package jpashop.jpabook;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaStudyApplication {
@@ -10,4 +12,9 @@ public class JpaStudyApplication {
         SpringApplication.run(JpaStudyApplication.class, args);
     }
 
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        //hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return new Hibernate5Module();
+    }
 }
