dd13bf0734573524a7832c82d8f241f25c6de068
SeanSin <seansin@chungbuk.ac.kr>
2020-08-14 17:12:16 +0900

    study2: implementated order simple search API version2
    기존 엔티티를 외부 노출시키는 예제에서 DTO를 활용하는 예제로 구현

--- jpashop-api/src/main/java/jpashop/jpabook/api/OrderSimpleApiController.java
+++ jpashop-api/src/main/java/jpashop/jpabook/api/OrderSimpleApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 주문 조회 단순 REST 컨트롤러
@@ -33,5 +34,31 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByQueryDSL(new OrderSearch());
+
+        return orders.stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
